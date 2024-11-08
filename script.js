1d5561a95ab171bd4b6956d417535ad0d6ac724c
D Tarnawski <dtarnawskilts@gmail.com>
2024-11-08 11:19:50 -0600

    added some comments and fleshed out about page

--- script.js
+++ script.js
@@ -6,24 +6,28 @@ function runCmdLine() {
     //console.log(commandLine.value);
 
     let userCMD = commandLine.value.toLowerCase().match(/("[^"]+"|[^"\s]+)/g);
-
     console.log(userCMD);
 
 
 
     switch (userCMD[0]) {
+        //display list of available commands
         case "help":
             output.innerHTML = "Here's a list of commands:<br>cd - navigate to different pages<br>contact - get contact info<br>help - pull up list of available commands";
             break;
-
+        //retrieve contact info
         case "contact":
             if (userCMD[1] == undefined) {
                 output.innerHTML = "Here's some of my contact info:<br>";
+            } else if (userCMD[1] == "email") {
+                output.innerHTML = "You can email me at <a href=\"mailto:dtarnawskilts@gmail.com\">dtarnawskilts@gmail.com<a>.";
+            } else if (userCMD[1] == "twitter" || userCMD[1] == "x") {
+                output.innerHTML = "I'm not currently on twitter!";
             } else {
-                output.innerHTML = "Error: " + userCMD[1] + " is not a valid argument for 'contact'.";
+                output.innerHTML = "Error: " + userCMD[1] + " is not a valid argument for \"contact\".";
             }
             break;
-        
+        //navigate between site pages
         case "cd":
             if (userCMD[1] == "home") {
                 window.location.href = "index.html";
@@ -34,14 +38,14 @@ function runCmdLine() {
             } else if (userCMD[1] == "contact") {
                 window.location.href = "contact.html";
             } else if (userCMD[1] == undefined) {
-                output.innerHTML = "Error: Destination not specified";
+                output.innerHTML = "Error: Destination for \"cd\" not specified";
             } else {
-                output.innerHTML = "Error: " + userCMD[1] + " is not a valid argument for 'cd'.";
+                output.innerHTML = "Error: " + userCMD[1] + " is not a valid argument for \"cd\".";
             }
             break;
 
         default:
-            output.innerHTML = "Error: " + userCMD[0] + " is not a recognized command.<br>Type 'help' for a list of commands.";
+            output.innerHTML = "Error: " + userCMD[0] + " is not a recognized command.<br>Type \"help\" for a list of commands.";
 
     }
 }
